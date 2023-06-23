1247b8c0e98bb75c5e9b8f9e2a283f5788e08b4d
Domitius <133902698+recenasu@users.noreply.github.com>
2023-06-22 21:49:43 -0700

    added a script file

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var myName;
+var myColor;
+var myFavorite;
+var firstValue;
+var secondValue;
+var thirdValue;
+var fourthValue;
+
+myName = "Rick";
+myColor = "Blue";
+myFavorite = "Blue";
+firstValue = 12;
+secondValue = 14;
+thirdValue = true;
+fourthValue = false;
+
+console.log(myName);
+console.log(myColor);
+console.log(myFavorite);
+console.log(myName == myColor);
+console.log(myColor == myFavorite);
+console.log(myColor + " " + myName + " " + myFavorite);
+console.log(1 / myColor);
+console.log(myColor / myColor);
+console.log(firstValue * secondValue);
+console.log(firstValue / secondValue);
+console.log(firstValue === secondValue);
+console.log(firstValue !== secondValue);
+console.log(thirdValue && fourthValue);
+console.log(thirdValue || fourthValue);
+
+
+
